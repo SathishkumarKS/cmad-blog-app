abdccb5f9a79864571c4d73c8466a421e20ab3ce
SathishkumarKS <sathish.hari@gmail.com>
2016-05-01 23:37:08 +0530

    Modify searchByTags to be case insensitive

--- blog-service/src/main/java/com/cisco/training/cmad/blog/dao/BlogDAOImpl.java
+++ blog-service/src/main/java/com/cisco/training/cmad/blog/dao/BlogDAOImpl.java
@@ -22,7 +22,7 @@ public class BlogDAOImpl extends BasicDAO<Blog, ObjectId> implements BlogDAO  {
 
     @Override
     public List<Blog> getByTag(String tagName) {
-        return createQuery().order("-createdAt").field("tags").contains(tagName).asList();
+        return createQuery().order("-createdAt").field("tags").containsIgnoreCase(tagName).asList();
     }
 
 }
